a6e69eab5c2826b533a05889290bb5c17fc4ca61
zhuang-93 <82987226+zhuang-93@users.noreply.github.com>
2021-08-16 15:38:22 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name="fbpcp",
+    name="ziyan-test-fbpcp",
     version="0.1.0",
     description="Facebook Private Computation Platform",
     author="Facebook",
